f0037180eab527640fa2e7b0d3fac3f2fe43d036
Aaron <pag87@nau.edu>
2020-01-27 12:23:52 -0700

    added comments, changed variable names and cleaned up code slightly

--- EchoServer.java
+++ EchoServer.java
@@ -44,10 +44,9 @@ class EchoThread implements Runnable
     private int count;
     InputStream io;
     OutputStream os;
-    private DataOutputStream out;
+    private DataOutputStream toClient;
 	private byte[] inData;
 	private int state;
-	private boolean connected;
 
     EchoThread(Socket clientSocket, int count)
     {
@@ -56,6 +55,7 @@ class EchoThread implements Runnable
 		inData = new byte[1];
 		state = START_STATE;
     }
+
 
 	public static boolean isValidChar(char character)
     {
@@ -70,24 +70,32 @@ class EchoThread implements Runnable
 	    return false;
     }
 
+	// updateState method takes in a state and character, returns the state
+	// resulting from processing character in state.
     public static int updateState(int state, char character )
     {
+		// any time a q is seen, go to SEEN_Q state.
 	    if (character == 'q')
 	    {
 		    return SEEN_Q;
 	    }
+		// if in SEEN_Q state see a q, advance to SEEN_QU state.
 	    if (state == SEEN_Q && character == 'u')
 	    {
 		    return SEEN_QU;
 	    }
+		// if in SEEN_QU state and see an i, advance to SEEN_QUI state.
 	    if (state == SEEN_QU && character == 'i')
 	    {
 	 	    return SEEN_QUI;
 	    }
+		// if in SEEN_QUI state and see a t, advance to FINAL_STATE.
 	    if (state == SEEN_QUI && character == 't')
 	    {
 		    return FINAL_STATE;
 	    }
+
+		// default back to start state.
 	    return START_STATE;
     }
 
@@ -102,31 +110,42 @@ class EchoThread implements Runnable
           os = clientSocket.getOutputStream();
         }
         catch (IOException e) {System.err.println(e);}
+
+		// open a data input stream from the client.
         try (DataInputStream fromClient = new DataInputStream(io))
         {
-            out = new DataOutputStream(os);
-			connected = true;
-            out.writeBytes("Connected");
+			// create a data output stream to the client
+            toClient = new DataOutputStream(os);
 
+			//send connection message to client
+            toClient.writeBytes("Connected");
+
+			// loop while a byte is read from the client,
+			// store byte in the first element of inData array.
             while(fromClient.read(inData, 0, 1) != 0)
             {
-                char toClient = (char) inData[0];
 
-				if(isValidChar(toClient))
+				// get the current byte from inData, convert to char.
+                char charFromClient = (char) inData[0];
+
+				// check if current char is valid
+				if(isValidChar(charFromClient))
 				{
-					System.out.println("Client "+count+": "+ toClient);
-                	out.writeBytes("Echo: "+ toClient + "\n");
-					state = updateState(state, toClient);
+					// server client connection display
+					System.out.println("Client "+count+": "+ charFromClient);
+
+					// send echo back to client.
+                	toClient.writeBytes("Echo: "+ charFromClient + "\n");
+
+					// update state machine to check for 'quit' sequence.
+					state = updateState(state, charFromClient);
 				}
 
+				// if current char puts us in final state (seen 'quit'),
+				// send disconnection message, exit loop.
 				if (state == FINAL_STATE)
 				{
-					connected = false;
-				}
-
-				if (!connected)
-				{
-					out.writeBytes("Disconnected.\n");
+					toClient.writeBytes("Disconnected.\n");
 					break;
 				}
             }
@@ -135,6 +154,7 @@ class EchoThread implements Runnable
 		{
 			System.err.println(e);
 		}
+		// display message to server specifying that the client left.
         finally {
             System.out.println("Client " + count + " left.");
         }
